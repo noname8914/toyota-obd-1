ec883c1945c8d2574da06f3169661d1ae7b9842a
noname8914 <76901068+noname8914@users.noreply.github.com>
2021-01-04 01:48:06 +0900

    Update DEFINES.h

--- DEFINES.h
+++ DEFINES.h
@@ -1,6 +1,13 @@
 #define Ncyl 4.0      //4 форсунок
 #define Ninjection 1  //впрыск 1 раз за цикл
 
+    //#if defined(SDCARD) //DEFINE модуля записи на SD карту
+      //#include "SdFat.h"
+      //#include <SPI.h>
+      //#define SS 5 // Номер ноги SS SD модуля
+      //#define FILE_BASE_NAME "Data"   //шаблон имени файла
+      //#define error(msg) sd.errorHalt(F(msg)) //ошибки при работе с SD
+    //#endif
 
 //DEFINE OBD READER
 #define  MY_HIGH  HIGH //LOW    // I have inverted the Eng line using an Opto-Coupler, if yours isn't then reverse these low & high defines.
@@ -17,3 +24,6 @@
 #define OBD_OXSENS 9 // Лямбда 1
 //#define OBD_OXSENS2 10 // Лямбда 2 на V-образных движка. У меня ее нету.
 
+    //#if defined(INJECTOR)
+      //по сигналу с форсунок
+    //#endif
